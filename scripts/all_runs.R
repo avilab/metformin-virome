3201fc2a20563f1d80300a4a634d9b74f5fb266a
Taavi Päll <tapa741@gmail.com>
2019-11-13 17:04:26 +0200

    download run info from ENA

---
+++ scripts/all_runs.R
@@ -0,0 +1,20 @@
+library(glue)
+library(tidyverse)
+library(here)
+
+# Getting run info from ENA: sample names and ftp addresses
+bioproject <- c("PRJEB1786", "PRJEB5224", "PRJEB4336")
+fields <- "study_accession,sample_accession,sample_title,sample_alias,run_accession,read_count,fastq_ftp"
+url <- glue("https://www.ebi.ac.uk/ena/portal/api/filereport?accession={bioproject}&result=read_run&fields={fields}&format=tsv&download=true")
+out <- map2(url, bioproject, ~download.file(url = .x, destfile = glue("output/{.y}.tsv")))
+
+runs <- glue("output/{bioproject}.tsv") %>% 
+  map(read_tsv, col_types = "cccccdc") %>% 
+  bind_rows()
+runs %>% 
+  mutate(sample_name = case_when(
+    study_accession == "PRJEB1786" ~ sample_title,
+    TRUE ~ sample_alias
+  )) %>% 
+  select(study_accession, sample_accession, sample_name, run_accession, read_count, fastq_ftp) %>% 
+  write_csv(here("output/all_runs.csv"))
