209b3f940527717a76366f64d10f1719ca1c08a7
Taavi Päll <tapa741@gmail.com>
2019-11-14 17:41:01 +0200

    metformin runs

--- scripts/import_sample_metadata.R
+++ scripts/import_sample_metadata.R
@@ -6,8 +6,14 @@ SItableS1_sample_subsets <- read_excel("data/nature15766-s2/SItableS1.xls", shee
 SItableS1_phenotypes <- read_excel("data/nature15766-s2/SItableS1.xls", sheet = "Phenotypes", na = "NA")
 SItableS1_sample_subsets %>% 
   filter(Status %in% c("T2D metformin-", "T2D metformin+")) %>% 
-  left_join(SItableS1_phenotypes) %>% 
-  count(Status)
+  count(Status) %>% 
+  knitr::kable(caption = "Number of T2D patiens on metformin treatment.") %>% 
+  kableExtra::kable_styling()
+
+nature15766_t2d <- SItableS1_sample_subsets %>% 
+  filter(Status %in% c("T2D metformin-", "T2D metformin+")) %>% 
+  select(sample_name = Sample, Status) %>% 
+  mutate(sample_name = str_remove(sample_name, "NG.*_"))
 
 nature12198_s2 <- read_excel("data/nature12198-s2.xlsx", sheet = "Supplementary Table 3", skip = 1) %>% 
   rename(Sample = `Sample ID`, oral_anti_diabetic_medication = `Oral anti-diabetic medication (-, no medication; Met, metformin; Sulph, sulphonylurea)`) %>% 
@@ -15,4 +21,34 @@ nature12198_s2 <- read_excel("data/nature12198-s2.xlsx", sheet = "Supplementary 
 nature12198_s2 %>% 
   filter(Classification == "T2D") %>% 
   select(Sample, Classification, oral_anti_diabetic_medication) %>% 
-  count(oral_anti_diabetic_medication)
+  count(oral_anti_diabetic_medication) %>% 
+  knitr::kable(caption = "Number of T2D patiens on metformin treatment.") %>% 
+  kableExtra::kable_styling()
+
+nature12198_t2d <- nature12198_s2 %>% 
+  filter(Classification == "T2D", oral_anti_diabetic_medication %in% c("-", "Met")) %>% 
+  mutate(Status = case_when(
+    oral_anti_diabetic_medication == "-" ~ "T2D metformin-",
+    oral_anti_diabetic_medication == "Met" ~ "T2D metformin+"
+  )) %>% 
+  select(sample_name = Sample, Status)
+
+t2d_samples <- bind_rows(nature15766_t2d, nature12198_t2d)
+
+all_runs <- read_csv(here("output/all_runs.csv"))
+
+t2d_runs <- inner_join(t2d_samples, all_runs) %>% 
+  group_by(sample_name) %>% 
+  top_n(1, read_count) %>% 
+  distinct() %>% 
+  ungroup()
+t2d_runs %>% 
+  ggplot() +
+  geom_histogram(aes(read_count), binwidth = 1e6)
+
+# Runs/samples with more than 15M reads
+t2d_runs %>% 
+  filter(read_count >= 1e7) %>% 
+  count(Status)
+
+
